6240b47d1010e6e116ea2cf8a50fbd412d6928bf
youngmin <rhans123@naver.com>
2021-11-11 17:42:38 +0900

    mainend

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const mainVisualSlider = new Swiper("#mainVisual", {
+  autoplay: true,
+  speed: 1000,
+  effect: "fade",
+  loop: true,
+  pagination: {
+    el: "#mainVisual .pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+const productSlider = new Swiper("#product .list", {
+  speed: 600,
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  navigation: {
+    prevE1: "#product .prev",
+    nextE1: "#product .next",
+  },
+});
